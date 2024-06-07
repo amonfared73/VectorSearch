8c7ba6ce77e65fe98069e26a03962756db2235da
Amir Monfared <amonfared73@gmail.com>
2024-06-07 12:27:19 +0430

    implementing word controller

---
+++ VectorSearch.ApplicationService/Services/IWordService.cs
@@ -0,0 +1,13 @@
+﻿using VectorSearch.Core.Models;
+
+namespace VectorSearch.ApplicationService.Services
+{
+    public interface IWordService
+    {
+        Task<IQueryable<Word>> GetAll();
+        Task<Word> GetById(int id);
+        Task<IQueryable<Word>> VectorSearch(string word);
+        Task Insert(string word);
+        Task Delete(string word);
+    }
+}
---
+++ VectorSearch.EndPoint/Controllers/WordController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VectorSearch.ApplicationService.Services;
+
+namespace VectorSearch.EndPoint.Controllers
+{
+    public class WordController : ControllerBase
+    {
+        private readonly IWordService _wordService;
+        public WordController(IWordService wordService)
+        {
+            _wordService = wordService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var result = await _wordService.GetAll();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetById(int id)
+        {
+            try
+            {
+                var result = await _wordService.GetById(id);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> VectorSearch(string word)
+        {
+            try
+            {
+                var result = await _wordService.VectorSearch(word);
+                return Ok(result);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> Insert(string word)
+        {
+            try
+            {
+                await _wordService.Insert(word);
+                return Ok($"Word: {word} inserted successfully!");
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Delete(string word)
+        {
+            try
+            {
+                await _wordService.Delete(word);
+                return Ok($"Word: {word} deleted successfully!");
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
