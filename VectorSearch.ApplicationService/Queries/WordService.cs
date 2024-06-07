b0e8d6da8e8aa9f9bc493f29646d480b02341b97
Amir Monfared <amonfared73@gmail.com>
2024-06-07 12:42:01 +0430

    Implement services

--- VectorSearch.ApplicationService/Queries/WordService.cs
+++ VectorSearch.ApplicationService/Queries/WordService.cs
@@ -1,4 +1,5 @@
-﻿using VectorSearch.ApplicationService.Services;
+﻿using Microsoft.EntityFrameworkCore;
+using VectorSearch.ApplicationService.Services;
 using VectorSearch.Core.Models;
 using VectorSearch.EntityFramework.DbContexts;
 
@@ -11,29 +12,61 @@ namespace VectorSearch.ApplicationService.Queries
         {
             _dbContextFactory = dbContextFactory;
         }
-
-        public Task<IQueryable<Word>> GetAll(string? word)
+        public async Task<IQueryable<Word>> GetAll(string? word)
         {
-            throw new NotImplementedException();
+            using (var context = _dbContextFactory.CreateDbContext())
+            {
+                var words = await context
+                    .Words
+                    .Where(w => string.IsNullOrEmpty(word) || w.Text.Contains(word))
+                    .ToListAsync();
+
+                return words.AsQueryable();
+            }
         }
 
-        public Task<Word> GetById(int id)
+        public async Task<Word> GetById(int id)
         {
-            throw new NotImplementedException();
+            using (var context = _dbContextFactory.CreateDbContext())
+            {
+                var word = await context
+                    .Words
+                    .FirstOrDefaultAsync(w => w.Id == id);
+
+                return word;
+            }
         }
 
-        public Task Insert(string word)
+        public async Task Insert(string word)
         {
-            throw new NotImplementedException();
+            using (var context = _dbContextFactory.CreateDbContext())
+            {
+                var words = context.Set<Word>();
+                await words.AddAsync(new Word()
+                {
+                    Text = word,
+                });
+                await context.SaveChangesAsync();
+            }
         }
 
         public Task<IQueryable<Word>> VectorSearch(string word)
         {
             throw new NotImplementedException();
         }
-        public Task Delete(string word)
+
+        public async Task Delete(string word)
         {
-            throw new NotImplementedException();
+            using (var context = _dbContextFactory.CreateDbContext())
+            {
+                var selectedWord = await context.Words.FirstOrDefaultAsync(w => w.Text.Contains(word));
+
+                if (selectedWord != null)
+                    throw new Exception($"Word: {word} not found!");
+
+                context.Words.Remove(selectedWord);
+                await context.SaveChangesAsync();
+            }
         }
     }
 }
