04037da063ada3762474f56fb1f3b2a1db78682b
Amir Monfared <amonfared73@gmail.com>
2025-01-03 20:18:20 +0330

    implement loading spinner

---
+++ VectorSearch.WPF/Tools/BoolToVisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace VectorSearch.WPF.Tools
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return boolValue ? Visibility.Visible : Visibility.Collapsed;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is Visibility visibility && visibility == Visibility.Visible;
+        }
+    }
+}
