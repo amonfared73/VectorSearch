339b813f91bf70bad738b22d01b965ebbbb138fe
Amir Monfared <amonfared73@gmail.com>
2025-01-03 09:47:47 +0330

    move startup window to app.xaml.cs

--- VectorSearch.WPF/App.xaml.cs
+++ VectorSearch.WPF/App.xaml.cs
@@ -9,6 +9,12 @@ namespace VectorSearch.WPF
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
     }
 
 }
