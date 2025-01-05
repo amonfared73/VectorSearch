688fb32c4b28d298e447a8cf7d42e4e95e28b214
Amir Monfared <amonfared73@gmail.com>
2025-01-05 20:24:08 +0330

    introducing helper methods

---
+++ VectorSearch.EF/Tools/VectorParser.cs
@@ -0,0 +1,17 @@
+﻿namespace VectorSearch.EF.Tools
+{
+    public static class VectorParser
+    {
+        public static double[] ParseVector(this string vectorString)
+        {
+            return vectorString.Split(' ')
+                               .Select(double.Parse)
+                               .ToArray();
+        }
+
+        public static string SerializeVector(this double[] vector)
+        {
+            return string.Join(" ", vector);
+        }
+    }
+}
