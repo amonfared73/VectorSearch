d9ca1c5438a2bce6664171806f45b09efe3ccdca
Amir Monfared <amonfared73@gmail.com>
2025-01-06 21:51:23 +0330

    add PagedResultViewModel

---
+++ VectorSearch.Domain/ViewModels/PagedResultViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace VectorSearch.Domain.ViewModels
+{
+    public class PagedResultViewModel<T> where T : class
+    {
+        public IEnumerable<T> Data { get; set; }
+        public int CurrentPage { get; set; } = 1;
+        public int TotalPages { get; set; }
+        public long? TotalRecords { get; set; }
+    }
+}
