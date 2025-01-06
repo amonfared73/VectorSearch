9b101d5d61633c5a138cae0d550adf4b80e31fec
Amir Monfared <amonfared73@gmail.com>
2025-01-06 21:12:00 +0330

    add search options parameters

--- VectorSearch.Domain/DTOs/SearchOptions.cs
+++ VectorSearch.Domain/DTOs/SearchOptions.cs
@@ -4,5 +4,7 @@
     {
         public string? Text { get; set; } = string.Empty;
         public bool IsVectorSearchEnabled { get; set; } = false;
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 15;
     }
 }
