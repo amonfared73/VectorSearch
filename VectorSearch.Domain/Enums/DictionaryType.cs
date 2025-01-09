dabe612416e54655ffce1bce0aaebb227bf69d67
Amir Monfared <amonfared73@gmail.com>
2025-01-09 23:34:37 +0330

    add dictionary enum

---
+++ VectorSearch.Domain/Enums/DictionaryType.cs
@@ -0,0 +1,10 @@
+﻿namespace VectorSearch.Domain.Enums
+{
+    public enum DictionaryType
+    {
+        glove_6B_50d = 1,
+        glove_6B_100d = 2,
+        glove_6B_200d = 3,
+        glove_6B_300d = 4,
+    }
+}
