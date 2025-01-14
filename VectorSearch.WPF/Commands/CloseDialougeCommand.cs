550184200780424b83d2bcd01db8c83451dfa01a
Amir Monfared <amonfared73@gmail.com>
2025-01-14 22:35:47 +0330

    still working on it

--- VectorSearch.WPF/Commands/CloseDialougeCommand.cs
+++ VectorSearch.WPF/Commands/CloseDialougeCommand.cs
@@ -1,10 +1,18 @@
-﻿namespace VectorSearch.WPF.Commands
+﻿using VectorSearch.WPF.ViewModels;
+
+namespace VectorSearch.WPF.Commands
 {
     public class CloseDialougeCommand : CommandBase
     {
+        private readonly DialogBoxViewModel _dialogBoxViewModel;
+        public CloseDialougeCommand(DialogBoxViewModel dialogBoxViewModel)
+        {
+            _dialogBoxViewModel = dialogBoxViewModel;
+        }
+
         public override void Execute(object? parameter)
         {
-            throw new NotImplementedException();
+            _dialogBoxViewModel.IsVisible = false;
         }
     }
 }
--- VectorSearch.WPF/ViewModels/DialogBoxViewModel.cs
+++ VectorSearch.WPF/ViewModels/DialogBoxViewModel.cs
@@ -1,16 +1,18 @@
 ﻿using System.Windows.Input;
+using VectorSearch.WPF.Commands;
 
 namespace VectorSearch.WPF.ViewModels
 {
     public class DialogBoxViewModel : ViewModelBase
     {
+        public bool IsVisible { get; set; }
         public string Title { get; set; }
         public string Message { get; set; }
         public ICommand CloseCommand { get; set; }
         public string CloseText { get; set; }
         public DialogBoxViewModel()
         {
-            CloseCommand = null;
+            CloseCommand = new CloseDialougeCommand(this);
         }
     }
 }
