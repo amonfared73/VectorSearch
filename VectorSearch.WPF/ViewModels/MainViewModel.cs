cf27bb7062a86437402a130aabe755370515a499
Amir Monfared <amonfared73@gmail.com>
2025-01-14 09:13:52 +0330

    implementing simple navigation

---
+++ VectorSearch.WPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using VectorSearch.WPF.Stores;
+
+namespace VectorSearch.WPF.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private readonly VectorSearchStore _vectorSearchStore;
+        public ViewModelBase CurrentViewModel { get; }
+        public MainViewModel(VectorSearchStore vectorSearchStore)
+        {
+            _vectorSearchStore = vectorSearchStore;
+            CurrentViewModel = new VectorSearchViewModel(_vectorSearchStore);
+        }
+    }
+}
