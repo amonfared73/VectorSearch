005b88937756ebe31c491f2b892d7cc483d230f3
Amir Monfared <amonfared73@gmail.com>
2025-03-28 13:46:06 +0330

    Fixing python file

--- VectorSearch.LLM/Python/VectorGenerator.py
+++ VectorSearch.LLM/Python/VectorGenerator.py
@@ -1,13 +1,11 @@
-
 from fastapi import FastAPI
 from pydantic import BaseModel
 from sentence_transformers import SentenceTransformer
-from sklearn.decomposition import PCA
-import numpy as np
 
 app = FastAPI()
+
+# Load SentenceTransformer
 model = SentenceTransformer('heydariAI/persian-embeddings')
-pca = PCA(n_components=50) 
 
 class QueryModel(BaseModel):
     query: str
@@ -15,8 +13,7 @@ class QueryModel(BaseModel):
 @app.post("/generate_vector/")
 async def generate_vector(data: QueryModel):
     embedding = model.encode([data.query])[0]
-    reduced_vector = pca.transform(np.array([embedding]))[0]
-    return {"vector": reduced_vector.tolist()}
+    return {"vector": embedding.tolist()}
 
 if __name__ == "__main__":
     import uvicorn
