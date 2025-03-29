5023e83815769465592f26ed8ea62046f64fa9b5
Amir Monfared <amonfared73@gmail.com>
2025-03-29 12:24:07 +0330

    Remove comments

--- VectorSearch.LLM/Python/VectorGenerator.py
+++ VectorSearch.LLM/Python/VectorGenerator.py
@@ -6,21 +6,17 @@ import numpy as np
 
 app = FastAPI()
 
-# Load the Persian sentence transformer model
 model = SentenceTransformer('heydariAI/persian-embeddings')
 
-# Load the trained PCA model
-pca = joblib.load('pca_model.pkl')  # Ensure that 'pca_model.pkl' is in the correct path
+pca = joblib.load('pca_model.pkl')  
 
 class QueryModel(BaseModel):
     query: str
 
 @app.post("/generate_vector/")
 async def generate_vector(data: QueryModel):
-    # Generate the 1024D embedding from the input query
     embedding_1024d = model.encode([data.query])[0]
     
-    # Reduce the embedding to 50D using the PCA model
     embedding_50d = pca.transform(np.array(embedding_1024d).reshape(1, -1))[0]
     
     return {"vector": embedding_50d.tolist()}
