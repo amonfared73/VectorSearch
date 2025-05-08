a1ecaa7c6089f8748d0316b021cfbce8ee963529
Amir Monfared <amonfared73@gmail.com>
2025-05-08 23:33:40 +0330

    Add multiple company for vector generation

--- VectorSearch.LLM/Python/VectorGenerator.py
+++ VectorSearch.LLM/Python/VectorGenerator.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, HTTPException
 from pydantic import BaseModel
 from sentence_transformers import SentenceTransformer
 import joblib
@@ -8,18 +8,41 @@ app = FastAPI()
 
 model = SentenceTransformer('heydariAI/persian-embeddings')
 
-pca = joblib.load('pca_model.pkl')  
+# pca = joblib.load('pca_model.pkl')  
+
+pca_models = {
+    "digiKala": "pca_model.pkl",
+    "faranShimi": "268_FaranShimi.pkl",
+    "psg": "117_PSG.pkl",
+}
+
+loaded_pcas = {
+    name: joblib.load(filename) for name, filename in pca_models.items()
+}
 
 class QueryModel(BaseModel):
     query: str
 
+# @app.post("/generate_vector/")
+# async def generate_vector(data: QueryModel):
+#     embedding_1024d = model.encode([data.query])[0]
+    
+#     embedding_50d = pca.transform(np.array(embedding_1024d).reshape(1, -1))[0]
+    
+#     return {"vector": embedding_50d.tolist()}
+
 @app.post("/generate_vector/")
-async def generate_vector(data: QueryModel):
-    embedding_1024d = model.encode([data.query])[0]
-    
-    embedding_50d = pca.transform(np.array(embedding_1024d).reshape(1, -1))[0]
-    
-    return {"vector": embedding_50d.tolist()}
+async def generate_vector(data: QueryModel, company: str = Query(...)):
+    if company not in loaded_pcas:
+        raise HTTPException(status_code=404, detail=f"PCA model not found for company '{company}'")
+
+    try:
+        embedding_1024d = model.encode([data.query])[0]
+        pca = loaded_pcas[company]
+        embedding_50d = pca.transform(np.array(embedding_1024d).reshape(1, -1))[0]
+        return {"vector": embedding_50d.tolist()}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
     import uvicorn
